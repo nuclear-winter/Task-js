b37e17d0d35f99fadcd2fcdbce7f5725b52fa6c4
nuclear-winter <41743787+nuclear-winter@users.noreply.github.com>
2018-09-07 22:50:16 +0300

    Create 1. evenA.js
    If a is an even number, a * b, otherwise a + b

---
+++ Base tasks/conditional statements/1. evenA.js
@@ -0,0 +1,13 @@
+function evenA (a, b) {
+    
+    if (a % 2 == 0) {
+        a * b;
+        console.log("a = " + a  + ": это чётное число!");
+    } else {
+        a + b;
+        console.log("a = " + a + ": это нечётное число!");
+    }
+
+};
+
+evenA(7):
